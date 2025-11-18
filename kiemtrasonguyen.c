b34e7a89a4eed048f28d88ab1db4b1166da2badc
Võ Minh Hùng <hungvo081207@gmail.com>
2025-11-18 10:55:13 +0700

    Implement prime number checking in kiemtrasonguyen.c
    Add a program to check if a number is prime

---
+++ kiemtrasonguyen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x, i, dem = 0;
+    printf("Nhap so x: ");
+    scanf("%d", &x);
+
+    if (x < 2) {
+        printf("%d khong phai la so nguyen to.\n", x);
+    } else {
+        for (i = 2; i <= sqrt(x); i++) {
+            if (x % i == 0) {
+                dem++;
+                break;
+            }
+        }
+    }
+        if (dem == 0){
+            printf("%d la so nguyen to.\n", x);
+        }
+        else{
+            printf("%d không phải là số nguyên tố.", x);
+        }
+}
